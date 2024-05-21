fc27cb08a690b52fcc6f12d6008db88701bc0dda
CarloMechatronics <carlo.torres@utec.edu.pe>
2024-05-21 14:17:06 -0500

    First commit

---
+++ src/test/java/com/reserve/hackaton/HackatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reserve.hackaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
